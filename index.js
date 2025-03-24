b1fb5905683ae216420b9c842f2fc395faa69ea6
thomasfromcopernic <thomas@agence-copernic.fr>
2025-03-24 10:28:34 +0100

    Add url and index.js

--- index.js
+++ index.js
@@ -1,26 +1,77 @@
-const express = require('express');
-const axios = require('axios');
+const express = require("express");
+const axios = require("axios");
 const app = express();
+require("dotenv").config();
 
-app.set('view engine', 'pug');
-app.use(express.static(__dirname + '/public'));
+app.set("view engine", "pug");
+app.use(express.static(__dirname + "/public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // * Please DO NOT INCLUDE the private app access token in your repo. Don't do this practicum in your normal account.
-const PRIVATE_APP_ACCESS = '';
+const PRIVATE_APP_ACCESS = process.env.PRIVATE_APP_ACCESS;
+const HUBSPOT_BASE_URL = "https://api.hubapi.com/crm/v3/objects";
+const CUSTOM_OBJECT_ID = "2-140708332";
 
 // TODO: ROUTE 1 - Create a new app.get route for the homepage to call your custom object data. Pass this data along to the front-end and create a new pug template in the views folder.
 
 // * Code for Route 1 goes here
 
+app.get("/", async (req, res) => {
+  const url = `${HUBSPOT_BASE_URL}/${CUSTOM_OBJECT_ID}?properties=nom,pv,attaque`;
+  const headers = {
+    Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
+    "Content-Type": "application/json",
+  };
+
+  try {
+    const response = await axios.get(url, { headers });
+    const data = response.data.results;
+    console.log(data);
+    res.render("homepage", { title: "Custom Objects | HubSpot APIs", data });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Erreur lors de la récupération des données");
+  }
+});
+
 // TODO: ROUTE 2 - Create a new app.get route for the form to create or update new custom object data. Send this data along in the next route.
 
 // * Code for Route 2 goes here
+app.get("/update-cobj", (req, res) => {
+  res.render("updates", { title: "Update Custom Object Form | HubSpot" });
+});
 
 // TODO: ROUTE 3 - Create a new app.post route for the custom objects form to create or update your custom object data. Once executed, redirect the user to the homepage.
 
 // * Code for Route 3 goes here
+
+// ROUTE 3 - Création d'un nouvel objet personnalisé
+app.post("/update-cobj", async (req, res) => {
+  const newObject = {
+    properties: {
+      nom: req.body.nom,
+      pv: req.body.pv,
+      attaque: req.body.attaque,
+    },
+  };
+
+  const url = `${HUBSPOT_BASE_URL}/${CUSTOM_OBJECT_ID}`;
+  const headers = {
+    Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
+    "Content-Type": "application/json",
+  };
+
+  try {
+    await axios.post(url, newObject, { headers });
+    res.redirect("/");
+  } catch (error) {
+    console.error(error.response);
+    res
+      .status(500)
+      .send("Erreur lors de la création du nouvel objet personnalisé");
+  }
+});
 
 /** 
 * * This is sample code to give you a reference for how you should structure your calls. 
@@ -66,6 +117,5 @@ app.post('/update', async (req, res) => {
 });
 */
 
-
 // * Localhost
-app.listen(3000, () => console.log('Listening on http://localhost:3000'));
+app.listen(3000, () => console.log("Listening on http://localhost:3000"));
